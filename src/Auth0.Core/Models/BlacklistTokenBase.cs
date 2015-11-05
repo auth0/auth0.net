a38f0ef6f6dd51b63d2d170563402748aa25d723
Jerrie Pelser <jerrie@jerriepelser.com>
2015-11-05 14:52:37 +0700

    Create blacklist models

---
+++ src/Auth0.Core/Models/BlacklistTokenBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Auth0.Core.Models
+{
+    public class BlacklistTokenBase
+    {
+        /// <summary>
+        /// Gets or sets the JWT's aud claim. The Client identifier of the client for which it was issued.
+        /// </summary>
+        public string Aud { get; set; }
+
+        /// <summary>
+        /// Gets or sets the jti of the JWT to be blacklisted.
+        /// </summary>
+        public string Jti { get; set; }        
+    }
+}
