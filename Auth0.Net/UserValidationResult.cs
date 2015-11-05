d40fda8a9d5d6dd7821650e542dc328d23c014c5
Jerrie Pelser <jerrie@jerriepelser.com>
2015-11-05 12:06:46 +0700

    Remove existing project

--- Auth0.Net/UserValidationResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Auth0
-{
-    public class UserValidationResult
-    {
-        public bool IsValid { get; internal set; }
-
-        public string Message { get; internal set; }
-    }
-}
--- examples/webapi/Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
