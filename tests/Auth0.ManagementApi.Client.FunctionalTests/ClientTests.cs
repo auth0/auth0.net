096461fe336beeb0a398baed01c43c86e91ba50f
Jerrie Pelser <jerrie@jerriepelser.com>
2015-11-20 14:33:23 +0700

    First functional test. Checking in to see whether it works on AppVeyor

---
+++ tests/Auth0.ManagementApi.Client.FunctionalTests/ClientTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Auth0.ManagementApi.Client.FunctionalTests
+{
+    [TestClass]
+    public class ClientTests : TestBase
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var apiClient = new ManagementApiClient(GetVariable("AUTH_TOKEN_CLIENTS"), new Uri(GetVariable("AUTH0_API_URL")));
+
+            // Get all clients
+            var clients = await apiClient.Clients.GetAll();
+
+            // Check we are getting clients
+            Assert.IsNotNull(clients);
+        }
+    }
+}
