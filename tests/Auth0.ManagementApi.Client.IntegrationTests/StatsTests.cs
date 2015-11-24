eb38238c17d2dad3a1d68faea72c4b9da8fb76a0
Jerrie Pelser <jerrie@jerriepelser.com>
2015-11-24 14:42:18 +0700

    Add stats integration tests

---
+++ tests/Auth0.ManagementApi.Client.IntegrationTests/StatsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Auth0.ManagementApi.Client.IntegrationTests
+{
+    [TestFixture]
+    public class StatsTests : TestBase
+    {
+        [Test]
+        public async Task Test_stats_sequence()
+        {
+            var apiClient = new ManagementApiClient(GetVariable("AUTH0_TOKEN_STATS"), new Uri(GetVariable("AUTH0_API_URL")));
+
+            // Get stats for the past 10 days
+            var dailyStats = await apiClient.Stats.GetDailyStats(DateTime.Now.AddDays(-10), DateTime.Now);
+            dailyStats.Should().NotBeNull();
+
+            // Get the active users
+            var activeUsers = await apiClient.Stats.GetActiveUsers();
+            activeUsers.Should().BeGreaterOrEqualTo(0);
+        }
+    }
+}
