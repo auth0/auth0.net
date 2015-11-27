4a2d477d36d3020caca6791cf28b42cbb9de374f
Robert McLaws <robert@advancedrei.com>
2015-11-27 17:35:25 -0500

    Added missing normalized user profile properties.

--- src/Auth0.Core/Models/Identity.cs
+++ src/Auth0.Core/Models/Identity.cs
@@ -2,19 +2,37 @@
 
 namespace Auth0.Core.Models
 {
+
+    /// <summary>
+    /// Describes a 3rd party account for a given <see cref="User"/>.
+    /// </summary>
+    /// <remarks>
+    /// A single <see cref="User"/> may be linked to multiple 3rd party accounts. This object defines the details of one of those accounts.
+    /// </remarks>
+    [JsonObject]
     public class Identity
     {
+
+        /// <summary>
+        /// The token that can be used to call the <see cref="Provider"/>'s API to get more information about the user.
+        /// </summary>
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
         /// <summary>
         /// The name of the connection for the identity.
         /// </summary>
+        /// <remarks>
+        /// Sometimes, this is the same as the connection, but not always.
+        /// </remarks>
         [JsonProperty("connection")]
         public string Connection { get; set; }
 
         /// <summary>
-        /// The user's identifier.
+        /// Indicates whether this is a social identity.
         /// </summary>
-        [JsonProperty("user_id")]
-        public string UserId { get; set; }
+        [JsonProperty("isSocial")]
+        public bool? IsSocial { get; set; }
 
         /// <summary>
         /// The type of identity provider.
@@ -23,9 +41,10 @@ namespace Auth0.Core.Models
         public string Provider { get; set; }
 
         /// <summary>
-        /// Indicates whether this is a social identity.
+        /// The user's identifier.
         /// </summary>
-        [JsonProperty("isSocial")]
-        public bool? IsSocial { get; set; }
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+        
     }
 }
--- src/Auth0.Core/Models/User.cs
+++ src/Auth0.Core/Models/User.cs
@@ -3,8 +3,76 @@ using Newtonsoft.Json;
 
 namespace Auth0.Core.Models
 {
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class User : UserBase
     {
+
+        /// <summary>
+        /// Gets or sets the date the user was created.
+        /// </summary>
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// The first name of the user (if available).
+        /// </summary>
+        [JsonProperty("given_name")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The full name of the user (e.g.: John Foo). ALWAYS GENERATED.
+        /// </summary>
+        [JsonProperty("name")]
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// An array of objects with information about the user's identities. More than one will exists in case accounts are linked
+        /// </summary>
+        [JsonProperty("identities")]
+        public Identity[] Identities { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("last_ip")]
+        public string LastIpAddress { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("last_login")]
+        public DateTime LastLogin { get; set; }
+
+        /// <summary>
+        /// The last name of the user (if available).
+        /// </summary>
+        [JsonProperty("family_name")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Returned for the Facebook, Google, and Microsoft social providers.
+        /// </remarks>
+        [JsonProperty("locale")]
+        public string Locale { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("logins_count")]
+        public string LoginsCount { get; set; }
+
+        /// <summary>
+        /// URL pointing to the user picture (if not available, will use gravatar.com with the email). ALWAYS GENERATED
+        /// </summary>
+        [JsonProperty("picture")]
+        public string Picture { get; set; }
+
         /// <summary>
         /// Gets or sets whether the user's phone number is verified.
         /// </summary>
@@ -17,27 +85,16 @@ namespace Auth0.Core.Models
         public bool PhoneVerified { get; set; }
 
         /// <summary>
-        /// Gets or sets the user's unique identifier.
-        /// </summary>
-        [JsonProperty("user_id")]
-        public string UserId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the date the user was created.
-        /// </summary>
-        [JsonProperty("created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        /// <summary>
         /// Gets or sets the date the user was last updated (modified).
         /// </summary>
         [JsonProperty("updated_at")]
         public DateTime UpdatedAt { get; set; }
 
         /// <summary>
-        /// An array of objects with information about the user's identities. More than one will exists in case accounts are linked
+        /// A unique identifier of the user per identity provider, same for all apps (e.g.: google-oauth2|103547991597142817347). ALWAYS GENERATED
         /// </summary>
-        [JsonProperty("identities")]
-        public Identity[] Identities { get; set; }
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
     }
 }
