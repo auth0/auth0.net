83de639706f5561f6de168f2d0b4803035494c6b
Jerrie Pelser <jerrie@jerriepelser.com>
2015-11-30 08:40:48 +0700

    Add auth URI builder

--- src/Auth0.AuthenticationApi.Client/IAuthenticationApiClient.cs
+++ src/Auth0.AuthenticationApi.Client/IAuthenticationApiClient.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Auth0.AuthenticationApi.Client.Models;
 
 namespace Auth0.AuthenticationApi.Client
 {
     public interface IAuthenticationApiClient
     {
+        Task<Uri> BuildAuthorizationUri(BuildAuthorizationUriRequest request);
     }
 }
