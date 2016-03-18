69fd69b3b78a7b85b4a774186cc7e617f2849ea8
Menno Hamburg <menno.hamburg@quantler.com>
2016-03-18 14:11:27 +0100

    Added missing properties for additional usage
    Missing if you want to use refreshtokens

--- src/Auth0.AuthenticationApi/Models/AuthenticationResponse.cs
+++ src/Auth0.AuthenticationApi/Models/AuthenticationResponse.cs
@@ -1,4 +1,5 @@
-﻿namespace Auth0.AuthenticationApi.Models
+﻿using Newtonsoft.Json;
+namespace Auth0.AuthenticationApi.Models
 {
     /// <summary>
     /// Contains the response from an authentication request.
@@ -8,16 +9,25 @@
         /// <summary>
         /// Gets or sets the identifier token.
         /// </summary>
+        [JsonProperty("id_token")]
         public string IdToken { get; set; }
 
         /// <summary>
         /// Gets or sets the access token.
         /// </summary>
+        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
 
         /// <summary>
         /// Gets or sets the type of the token.
         /// </summary>
+        [JsonProperty("token_type")]
         public string TokenType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the refresh token
+        /// </summary>
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
     }
 }
