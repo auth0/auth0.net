7550db1216fa3e63f76ebffa8b91464f2ab51970
Nico Sabena <nico.sabena@hotmail.com>
2017-01-02 15:58:54 -0300

    Small changes on tests

--- tests/Auth0.AuthenticationApi.IntegrationTests/AccessTokenTests.cs
+++ tests/Auth0.AuthenticationApi.IntegrationTests/AccessTokenTests.cs
@@ -4,16 +4,69 @@ using Auth0.AuthenticationApi.Models;
 using Auth0.Tests.Shared;
 using FluentAssertions;
 using NUnit.Framework;
+using Auth0.ManagementApi;
+using Auth0.Core;
+using Auth0.ManagementApi.Models;
 
 namespace Auth0.AuthenticationApi.IntegrationTests
 {
     [TestFixture]
     public class AccessTokenTests : TestBase
     {
-        private string accessToken = "your access token";
+        private string socialAccessToken = "your google access token";
+        private ManagementApiClient managementApiClient;
+        private Connection connection;
+        private User newUser;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            var scopes = new
+            {
+                users = new
+                {
+                    actions = new string[] { "create", "delete" }
+                },
+                connections = new
+                {
+                    actions = new string[] { "create", "delete" }
+                }
+            };
+            string token = GenerateToken(scopes);
+
+            managementApiClient = new ManagementApiClient(token, new Uri(GetVariable("AUTH0_MANAGEMENT_API_URL")));
+
+            // We will need a connection to add the users to...
+            connection = await managementApiClient.Connections.CreateAsync(new ConnectionCreateRequest
+            {
+                Name = Guid.NewGuid().ToString("N"),
+                Strategy = "auth0",
+                EnabledClients = new[] { GetVariable("AUTH0_CLIENT_ID") }
+            });
+
+            // Add a new user
+            var newUserRequest = new UserCreateRequest
+            {
+                Connection = connection.Name,
+                Email = $"{Guid.NewGuid():N}@nonexistingdomain.aaa",
+                EmailVerified = true,
+                Password = "password"
+            };
+
+            newUser = await managementApiClient.Users.CreateAsync(newUserRequest);
+
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            if (connection != null)
+                await managementApiClient.Connections.DeleteAsync(connection.Id);
+        }
+
 
         [Test, Explicit]
-        public async Task Can_log_in_with_access_token()
+        public async Task Can_log_in_with_social_access_token()
         {
             var authenticationApiClient = new AuthenticationApiClient(new Uri(GetVariable("AUTH0_AUTHENTICATION_API_URL")));
 
@@ -21,7 +74,7 @@ namespace Auth0.AuthenticationApi.IntegrationTests
             {
                 ClientId = GetVariable("AUTH0_CLIENT_ID"),
                 Connection = "google-oauth2",
-                AccessToken = accessToken,
+                AccessToken = socialAccessToken,
                 Scope = "openid"
             });
 
@@ -36,11 +89,13 @@ namespace Auth0.AuthenticationApi.IntegrationTests
             var authenticationApiClient = new AuthenticationApiClient(new Uri(GetVariable("AUTH0_AUTHENTICATION_API_URL")));
             
             // First get the access token
-            var token = await authenticationApiClient.GetAccessTokenAsync(new AccessTokenRequest
+            var token = await authenticationApiClient.AuthenticateAsync(new AuthenticationRequest
             {
                 ClientId = GetVariable("AUTH0_CLIENT_ID"),
-                Connection = "google-oauth2",
-                AccessToken = accessToken,
+                ClientSecret = GetVariable("AUTH0_CLIENT_SECRET"),
+                Connection = connection.Name,
+                Username = newUser.Email,
+                Password = "password",
                 Scope = "openid"
             });
 
@@ -60,19 +115,21 @@ namespace Auth0.AuthenticationApi.IntegrationTests
         }
 
         [Test, Explicit]
+        [Ignore("updated_at is not a long value as expected. this will be fixed soon.")]
         public async Task Can_obtain_user_info()
         {
             var authenticationApiClient = new AuthenticationApiClient(new Uri(GetVariable("AUTH0_AUTHENTICATION_API_URL")));
 
             // First get the access token
-            var token = await authenticationApiClient.GetAccessTokenAsync(new AccessTokenRequest
+            var token = await authenticationApiClient.AuthenticateAsync(new AuthenticationRequest
             {
                 ClientId = GetVariable("AUTH0_CLIENT_ID"),
-                Connection = "google-oauth2",
-                AccessToken = accessToken,
-                Scope = "openid"
+                ClientSecret = GetVariable("AUTH0_CLIENT_SECRET"),
+                Connection = connection.Name,
+                Username = newUser.Email,
+                Password = "password",
+                Scope = "openid profile"
             });
-
 
             // Get the user info
             var user = await authenticationApiClient.GetUserInfoAsync(token.AccessToken);
@@ -86,14 +143,14 @@ namespace Auth0.AuthenticationApi.IntegrationTests
             var authenticationApiClient = new AuthenticationApiClient(new Uri(GetVariable("AUTH0_AUTHENTICATION_API_URL")));
 
             // First get the access token
-            var token = await authenticationApiClient.GetAccessTokenAsync(new AccessTokenRequest
+            var token = await authenticationApiClient.AuthenticateAsync(new AuthenticationRequest
             {
                 ClientId = GetVariable("AUTH0_CLIENT_ID"),
-                Connection = "google-oauth2",
-                AccessToken = accessToken,
+                Connection = connection.Name,
+                Username = newUser.Email,
+                Password = "password",
                 Scope = "openid"
             });
-
 
             // Get the user info
             var user = await authenticationApiClient.GetTokenInfoAsync(token.IdToken);
