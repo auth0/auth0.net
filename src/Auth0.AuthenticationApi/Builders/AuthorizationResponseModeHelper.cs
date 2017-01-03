42972f720108b01f75545b3f2ae48c23d19df277
Nico Sabena <nico.sabena@hotmail.com>
2017-01-03 12:30:47 -0300

    Added support for response_mode authorization parameter.

---
+++ src/Auth0.AuthenticationApi/Builders/AuthorizationResponseModeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth0.AuthenticationApi.Builders
+{
+    using Auth0.AuthenticationApi.Models;
+    
+    public class AuthorizationResponseModeHelper
+    {
+        private static Dictionary<AuthorizationResponseMode, string> Map = new Dictionary<AuthorizationResponseMode, string>
+        {
+            { AuthorizationResponseMode.FormPost, "form_post" }
+        };
+
+        public static string ConvertToString(AuthorizationResponseMode responseMode)
+        {
+            string value;
+            if (!Map.TryGetValue(responseMode, out value))
+            {
+                throw new ArgumentException("Unknown response type.", nameof(responseMode));
+            }
+
+            return value;
+        }
+    }
+}
