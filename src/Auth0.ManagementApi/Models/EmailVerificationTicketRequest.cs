3fd446d24e63b0cfcc22e05572fab8800278adbb
Tim Liang <tim.liang@perkinelmer.com>
2017-11-22 16:05:49 +0800

    Add ttl_sec property in email verification and password reset ticket request

--- src/Auth0.ManagementApi/Models/EmailVerificationTicketRequest.cs
+++ src/Auth0.ManagementApi/Models/EmailVerificationTicketRequest.cs
@@ -8,7 +8,6 @@ namespace Auth0.ManagementApi.Models
     /// </summary>
     public class EmailVerificationTicketRequest
     {
-
         /// <summary>
         /// The user will be redirected to this endpoint once the ticket is used.
         /// </summary>
@@ -21,6 +20,13 @@ namespace Auth0.ManagementApi.Models
         [JsonProperty("user_id")]
         public string UserId { get; set; }
 
+        /// <summary>
+        /// The ticket's lifetime in seconds starting from the moment of creation. 
+        /// After expiration the ticket can not be used to verify the users's email. 
+        /// If not specified or if you send 0 the Auth0 default lifetime will be applied
+        /// </summary>
+        [JsonProperty("ttl_sec")]
+        public int ExpiresIn { get; set; }
     }
 
 }
