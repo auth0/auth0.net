dbbe4e7343ffb9cd8d81ceba5f0cf8e0d777ee4b
Jerrie Pelser <jerrie@jerriepelser.com>
2018-07-09 09:20:38 +0700

    Update LoginRequestQuery with additional fields. Closes #193

--- src/Auth0.ManagementApi/Models/Rules/LoginRequestQuery.cs
+++ src/Auth0.ManagementApi/Models/Rules/LoginRequestQuery.cs
@@ -9,6 +9,11 @@ namespace Auth0.ManagementApi.Models.Rules
     [JsonObject]
     public class LoginRequestQuery
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("audience")]
+        public string Audience { get; set; }
 
         /// <summary>
         /// 
@@ -22,6 +27,18 @@ namespace Auth0.ManagementApi.Models.Rules
         [JsonProperty("connection")]
         public string Connection { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("device")]
+        public string Device { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("protocol")]
+        public string Protocol { get; set; }
+        
         /// <summary>
         /// 
         /// </summary>
@@ -40,6 +57,23 @@ namespace Auth0.ManagementApi.Models.Rules
         [JsonProperty("response_type")]
         public string ResponseType { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("sso")]
+        public string Sso { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("state")]
+        public string State { get; set; }
     }
 
 }
