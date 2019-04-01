feddedde9636b8b411108ba911b15427297a4697
Damien Guard <damieng@gmail.com>
2019-04-01 15:37:38 -0700

    Add role permission endpoints to RoleClient

--- src/Auth0.ManagementApi/Models/AssignUsersRequest.cs
+++ src/Auth0.ManagementApi/Models/AssignUsersRequest.cs
@@ -3,12 +3,12 @@ using Newtonsoft.Json;
 namespace Auth0.ManagementApi.Models
 {
     /// <summary>
-    ///
+    /// Contains details of users that should be assigned to a role.
     /// </summary>
     public class AssignUsersRequest
     {
         /// <summary>
-        /// User IDs to assign to the role
+        /// User IDs to assign to the role.
         /// </summary>
         [JsonProperty("users")]
         public string[] Users { get; set; }
---
+++ src/Auth0.ManagementApi/Models/AssociatePermissionsRequest.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Auth0.ManagementApi.Models
+{
+    /// <summary>
+    /// Contains details of permissions that should be assigned to a role.
+    /// </summary>
+    public class AssociatePermissionsRequest
+    {
+        /// <summary>
+        /// User IDs to assign to the role.
+        /// </summary>
+        [JsonProperty("permissions")]
+        public IList<PermissionIdentity> Permissions { get; set; }
+    }
+}
