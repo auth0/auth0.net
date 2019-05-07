a929b35016ab1303e5282c9d190666e95bbc7057
Damien Guard <damieng@gmail.com>
2019-05-07 15:17:06 -0700

    Add missing properties to ResourceServer

---
+++ src/Auth0.ManagementApi/Models/TokenDialect.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Auth0.ManagementApi.Models
+{
+    public enum TokenDialect
+    {
+        [EnumMember(Value = "access_token")]
+        AccessToken,
+
+        [EnumMember(Value = "access_token_authz")]
+        AccessTokenAuthZ
+    }
+}
