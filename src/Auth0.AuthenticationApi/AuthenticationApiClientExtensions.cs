76b77f52f37aedc72ff9177b36014e03e8803530
Damien Guard <damieng@gmail.com>
2019-11-22 11:46:37 -0800

    Split the API builders from client in a mostly compatible way

---
+++ src/Auth0.AuthenticationApi/AuthenticationApiClientExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Auth0.AuthenticationApi.Builders;
+
+namespace Auth0.AuthenticationApi
+{
+    public static class AuthenticationApiClientExtensions
+    {
+        /// <summary>
+        /// Creates a <see cref="AuthorizationUrlBuilder" /> for building an authorization URL.
+        /// </summary>
+        /// <param name="client"><see cref="IAuthenticationApiClient"/> capable of providing the Base URL.</param>
+        /// <returns>A new <see cref="AuthorizationUrlBuilder" /> configured for this baseUrl.</returns>
+        public static AuthorizationUrlBuilder BuildAuthorizationUrl(this IAuthenticationApiClient client)
+        {
+            return new AuthorizationUrlBuilder(client.BaseUri);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="LogoutUrlBuilder" /> for building a logout URL.
+        /// </summary>
+        /// <param name="client"><see cref="IAuthenticationApiClient"/> capable of providing the Base URL.</param>
+        /// <returns>A new <see cref="LogoutUrlBuilder" /> configured for this baseUrl.</returns>
+        public static LogoutUrlBuilder BuildLogoutUrl(this IAuthenticationApiClient client)
+        {
+            return new LogoutUrlBuilder(client.BaseUri);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="SamlUrlBuilder" /> for building a SAML authentication URL.
+        /// </summary>
+        /// <param name="client"><see cref="IAuthenticationApiClient"/> capable of providing the Base URL.</param>
+        /// <param name="clientId">ID of the client.</param>
+        /// <returns>A new <see cref="SamlUrlBuilder" /> configured for this baseUrl and <paramref name="client"/>.</returns>
+        public static SamlUrlBuilder BuildSamlUrl(this IAuthenticationApiClient client, string clientId)
+        {
+            return new SamlUrlBuilder(client.BaseUri, clientId);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="WsFedUrlBuilder" /> for building a WS-Federation authentication URL.
+        /// </summary>
+        /// <param name="client"><see cref="IAuthenticationApiClient"/> capable of providing the Base URL.</param>
+        /// <returns>A new <see cref="WsFedUrlBuilder" /> configured for this baseUrl.</returns>
+        public static WsFedUrlBuilder BuildWsFedUrl(this IAuthenticationApiClient client)
+        {
+            return new WsFedUrlBuilder(client.BaseUri);
+        }
+    }
+}
