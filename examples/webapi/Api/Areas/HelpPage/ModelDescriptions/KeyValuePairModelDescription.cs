2c3ff453feb4cb93d171fa88da2efcb73cc215c3
Damien Guard <damieng@gmail.com>
2020-01-13 08:52:50 -0800

    Remove old examples from repo

--- examples/webapi/Api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
