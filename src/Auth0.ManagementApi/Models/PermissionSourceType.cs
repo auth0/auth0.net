e074f5827e6046d09aef71ea07cafd237e3a929f
Nick <34492684+tec-nick@users.noreply.github.com>
2020-10-09 06:36:30 -0700

    Adds permission sources to User Response (#425)
    * Adds permission sources to User Response
    
    UsersClient now returns permissions along with the source of the permissions, according to apparent API specifications. This is done by simply updating the types to pass the permission sources from the http API to the client of this library.
    
    Tests received a minor update to include this change.
    
    * Fixes documentation on UserPermission
    
    Co-authored-by: Nick Mueller <NMueller@aerotec.com>
    Co-authored-by: Frederik Prijck <frederik.prijck@auth0.com>

---
+++ src/Auth0.ManagementApi/Models/PermissionSourceType.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Auth0.ManagementApi.Models
+{
+    /// <summary>
+    /// Types of permission sources
+    /// </summary>
+    public enum PermissionSourceType
+    {
+        /// <summary>
+        /// Permission assigned directly to user
+        /// </summary>
+        [EnumMember(Value = "direct")]
+        Direct,
+
+        /// <summary>
+        /// Permission assigned via role
+        /// </summary>
+        [EnumMember(Value = "role")]
+        Role
+    }
+}
