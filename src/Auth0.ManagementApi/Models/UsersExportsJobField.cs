39b7f25c9fd8a9a9a47d1269f58310beb6491e07
Frederik <frederik.prijck@gmail.com>
2020-10-16 09:58:27 +0200

    [SDK-1512] Implement the POST Job Users Export endpoint

---
+++ src/Auth0.ManagementApi/Models/UsersExportsJobField.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Auth0.ManagementApi.Models
+{
+    public class UsersExportsJobField
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("export_as")]
+        public string ExportAs { get; set; }
+    }
+}
---
+++ src/Auth0.ManagementApi/Models/UsersExportsJobFormat.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Auth0.ManagementApi.Models
+{
+    public enum UsersExportsJobFormat
+    {
+        [EnumMember(Value = "csv")]
+        CSV,
+        [EnumMember(Value = "json")]
+        Json
+    }
+}
