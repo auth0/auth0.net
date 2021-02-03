e447f5135255f2bca9d9467ef65f493f481ec718
Connor Conway <connor.conway@cloudhouse.com>
2021-02-03 19:31:48 +0000

    initialises Hooks

--- src/Auth0.ManagementApi/ManagementApiClient.cs
+++ src/Auth0.ManagementApi/ManagementApiClient.cs
@@ -147,6 +147,7 @@ namespace Auth0.ManagementApi
             EmailProvider = new EmailProviderClient(managementConnection, baseUri, defaultHeaders);
             EmailTemplates = new EmailTemplatesClient(managementConnection, baseUri, defaultHeaders);
             Guardian = new GuardianClient(managementConnection, baseUri, defaultHeaders);
+            Hooks = new HooksClient(managementConnection, baseUri, defaultHeaders);
             Jobs = new JobsClient(managementConnection, baseUri, defaultHeaders);
             Logs = new LogsClient(managementConnection, baseUri, defaultHeaders);
             LogStreams = new LogStreamsClient(managementConnection, baseUri, defaultHeaders);
