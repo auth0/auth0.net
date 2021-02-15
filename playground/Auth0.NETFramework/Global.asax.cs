8ff053c6fb8f99fe6a6b8480f8c63df9fc4076e7
Frederik Prijck <frederik.prijck@gmail.com>
2021-02-15 13:48:43 +0100

    Remove unused usings

--- playground/Auth0.NETFramework/Global.asax.cs
+++ playground/Auth0.NETFramework/Global.asax.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace Auth0.NETFramework
 {
