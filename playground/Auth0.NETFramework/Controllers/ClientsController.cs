cd368f6c2a8911737199b7fe09cfd1f520a5c0f8
Frederik Prijck <frederik.prijck@auth0.com>
2021-02-15 13:55:15 +0100

    [SDK-2297] Add playground applications for .NET 5, .NET Core 3.1 and .NET Framework (#472)
    * [SDK-2297] Add playground applications for .NET 5, .NET Core 3.1 and .NET Framework
    
    * Use dotnet 5
    
    * Migrate packages.config to PackageReferences
    
    * Remove playground samples from build
    
    * Remove unused usings

---
+++ playground/Auth0.NETFramework/Controllers/ClientsController.cs
@@ -0,0 +1,35 @@
+﻿using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Auth0.ManagementApi.Models;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Auth0.NETCore3.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private readonly AuthenticationApiClient _authClient;
+
+        public ClientsController()
+        {
+            _authClient = new AuthenticationApiClient(ConfigurationManager.AppSettings["Auth0Domain"]);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> Get()
+        {
+            var token = await _authClient.GetTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Audience = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/api/v2/",
+                ClientId = ConfigurationManager.AppSettings["Auth0ClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["Auth0ClientSecret"]
+            });
+
+            var mgmntClient = new ManagementApi.ManagementApiClient(token.AccessToken, ConfigurationManager.AppSettings["Auth0Domain"]);
+
+            return await mgmntClient.Clients.GetAllAsync(new GetClientsRequest(), new ManagementApi.Paging.PaginationInfo());
+        }
+    }
+}
