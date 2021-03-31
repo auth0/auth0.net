47521425a8c5c46db3eb1aa8132cb369dacc9d70
Frederik Prijck <frederik.prijck@gmail.com>
2021-03-31 16:25:34 +0200

    More refactors

--- src/Auth0.ManagementApi/Models/OrganizationCreateInvitationRequest.cs
+++ src/Auth0.ManagementApi/Models/OrganizationCreateInvitationRequest.cs
@@ -3,28 +3,6 @@ using System.Collections.Generic;
 
 namespace Auth0.ManagementApi.Models
 {
-    public class OrganizationGetAllRequest : OrganizationGetRequest
-    {
-        /// <summary>
-        /// Field to sort by. Use field:order where order is 1 for ascending and -1 for descending Defaults to created_at:-1.
-        /// </summary>
-        public string Sort { get; set; } = null;
-
-    }
-
-    public class OrganizationGetRequest
-    {
-        /// <summary>
-        /// A comma separated list of fields to include or exclude (depending on <see cref="IncludeFields"/>) from the result, empty to retrieve all fields.
-        /// </summary>
-        public string Fields { get; set; } = null;
-
-        /// <summary>
-        /// Specifies whether the fields specified in <see cref="Fields"/> should be included or excluded in the result.
-        /// </summary>
-        public bool? IncludeFields { get; set; } = null;
-
-    }
 
     /// <summary>
     /// Requests structure for creating a new organization invitation.
---
+++ src/Auth0.ManagementApi/Models/OrganizationGetAllRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Auth0.ManagementApi.Models
+{
+    public class OrganizationGetAllRequest : OrganizationGetRequest
+    {
+        /// <summary>
+        /// Field to sort by. Use field:order where order is 1 for ascending and -1 for descending Defaults to created_at:-1.
+        /// </summary>
+        public string Sort { get; set; } = null;
+
+    }
+}
