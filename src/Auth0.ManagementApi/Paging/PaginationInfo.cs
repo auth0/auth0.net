a57244c9076bff20c75ecc3d12262c0aefa8fd18
Evan Sims <hello@evansims.com>
2021-07-16 13:18:44 -0400

    code: Update PaginationInfo interface to support checkpoint pagination params.

--- src/Auth0.ManagementApi/Paging/PaginationInfo.cs
+++ src/Auth0.ManagementApi/Paging/PaginationInfo.cs
@@ -11,26 +11,40 @@
         /// <param name="pageNo">Page index of the results to return. First page is 0.</param>
         /// <param name="perPage">Number of results per page.</param>
         /// <param name="includeTotals">Whether to return the complete total result count (true) or not (false).</param>
-        public PaginationInfo(int pageNo = 0, int perPage = 50, bool includeTotals = false)
+        /// <param name="from">When using checkpoint pagination, an Id from which to begin the next page of results.</param>
+        /// <param name="take">When using checkpoint pagination, the number of results per page.</param>
+        public PaginationInfo(int? pageNo = 0, int? perPage = 50, bool? includeTotals = false, string? from = null, int take? = null)
         {
             IncludeTotals = includeTotals;
             PerPage = perPage;
             PageNo = pageNo;
+            From = from;
+            Take = take;
         }
 
         /// <summary>
         /// Return results with a total result count (true) or with no totals (false, default).
         /// </summary>
-        public bool IncludeTotals { get; }
+        public bool? IncludeTotals { get; } = null;
 
         /// <summary>
         /// Number of results per page.
         /// </summary>
-        public int PerPage { get; }
+        public int? PerPage { get; } = null;
 
         /// <summary>
         /// Page index of the results to return. First page is 0.
         /// </summary>
-        public int PageNo { get; }
+        public int? PageNo { get; } = null;
+
+        /// <summary>
+        /// When using checkpoint pagination, an Id from which to begin the next page of results.
+        /// </summary>
+        public string? From { get; } = null;
+
+        /// <summary>
+        /// When using checkpoint pagination, the number of results per page. Required to use checkpoint pagination.
+        /// </summary>
+        public int? Take { get; } = null;
     }
 }
