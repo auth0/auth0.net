34b7b5127f71d12f455a2ca45fd74d1a09046fda
Evan Sims <hello@evansims.com>
2021-07-16 13:18:56 -0400

    code: Update PagingInformation interface to support checkpoint pagination params.

--- src/Auth0.ManagementApi/Paging/PagingInformation.cs
+++ src/Auth0.ManagementApi/Paging/PagingInformation.cs
@@ -14,11 +14,11 @@ namespace Auth0.ManagementApi.Paging
         /// <remarks>
         /// This is either the `per_page` you requested if there
         /// are more results or a number smaller than that if this
-        /// is the last page of results and there are no more to 
+        /// is the last page of results and there are no more to
         /// page through.
         /// </remarks>
         [JsonProperty("length")]
-        public int Length { get; set; }
+        public int? Length { get; set; } = null;
 
         /// <summary>
         /// Maximum number of items requested.
@@ -27,7 +27,7 @@ namespace Auth0.ManagementApi.Paging
         /// This is the `per_page` you requested.
         /// </remarks>
         [JsonProperty("limit")]
-        public int Limit { get; set; }
+        public int? Limit { get; set; } = null;
 
         /// <summary>
         /// Start index into the number of items.
@@ -36,13 +36,19 @@ namespace Auth0.ManagementApi.Paging
         /// This is the `page` you requested multiplied by the `per_page`.
         /// </remarks>
         [JsonProperty("start")]
-        public int Start { get; set; }
+        public int? Start { get; set; } = null;
 
         /// <summary>
         /// Total number of items available on the server.
         /// </summary>
         [JsonProperty("total")]
-        public int Total { get; set; }
+        public int? Total { get; set; } = null;
+
+        /// <summary>
+        /// When using checkpoint pagination, the Id to use for supplemental paged requests.
+        /// </summary>
+        [JsonProperty("next")]
+        public string? Next { get; set; } = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PagingInformation"/> class
@@ -52,12 +58,14 @@ namespace Auth0.ManagementApi.Paging
         /// <param name="limit">Maximum number of items requested.</param>
         /// <param name="length">Number of items actually returned.</param>
         /// <param name="total">Total number of items available on the server.</param>
-        public PagingInformation(int start, int limit, int length, int total)
+        /// <param name="next">When using checkpoint pagination, the Id one should use for supplemental requests to receive more results.</param>
+        public PagingInformation(int? start = null, int? limit = null, int? length = null, int? total = null, string? next = null)
         {
             Start = start;
             Limit = limit;
             Length = length;
             Total = total;
+            Next = next;
         }
     }
 }
