e49bf825f245b5c071f4e6747e061d3998908db6
Frederik <frederik.prijck@gmail.com>
2021-07-29 13:41:04 +0200

    Ensure CustomDomainVerification.Methods can be serialized
    Fix #508

---
+++ src/Auth0.ManagementApi/Models/CustomDomainVerificationMethod.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Auth0.ManagementApi.Models
+{
+    /// <summary>
+    /// The custom domain verification method.
+    /// </summary>
+    public class CustomDomainVerificationMethod
+    {
+        /// <summary>
+        /// Domain verification method. ("cname" or "txt")
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value used to verify the domain.
+        /// </summary>
+        [JsonProperty("record")]
+        public string Record { get; set; }
+
+        /// <summary>
+        /// The name of the txt record for verification.
+        /// </summary>
+        [JsonProperty("domain")]
+        public string Domain { get; set; }
+    }
+}
