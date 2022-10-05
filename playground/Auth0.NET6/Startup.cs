845d67f7a1c917183399789a43cda6a6e028185f
Frederik Prijck <frederik.prijck@auth0.com>
2022-10-05 13:09:34 +0200

    Upgrade Tests and Playground to .NET6 (#594)

--- playground/Auth0.NET6/Startup.cs
+++ playground/Auth0.NET6/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Auth0.NET5
+namespace Auth0.NET6
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace Auth0.NET5
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth0.NET5", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth0.NET6", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace Auth0.NET5
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auth0.NET5 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auth0.NET6 v1"));
             }
 
             app.UseHttpsRedirection();
