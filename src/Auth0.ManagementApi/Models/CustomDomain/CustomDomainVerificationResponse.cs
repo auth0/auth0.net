6b173ee12bc66bca1c41114117cbf0dbf0e592b9
Travis Schlotfeldt <6312382+tmschlot@users.noreply.github.com>
2025-01-02 14:21:18 -0600

    Adds CnameApiKey to CustomDomainVerificationResponse

--- src/Auth0.ManagementApi/Models/CustomDomain/CustomDomainVerificationResponse.cs
+++ src/Auth0.ManagementApi/Models/CustomDomain/CustomDomainVerificationResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace Auth0.ManagementApi.Models
+﻿using Newtonsoft.Json;
+
+namespace Auth0.ManagementApi.Models
 {
     /// <summary>
     /// Response when requesting a custom domain verification
     /// </summary>
     public class CustomDomainVerificationResponse : CustomDomainBase
     {
-        
+        /// <summary>
+        /// The CNAME API key header value.
+        /// </summary>
+        [JsonProperty("cname_api_key")]
+        public string CnameApiKey { get; set; }
     }
 }
