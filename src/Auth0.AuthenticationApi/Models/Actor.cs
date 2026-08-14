32485ba816b7b7464d7a413c370011b832dac81c
Kailash B <kailash.b@okta.com>
2026-08-14 10:33:44 +0530

    chore: Address review comments

--- src/Auth0.AuthenticationApi/Models/Actor.cs
+++ src/Auth0.AuthenticationApi/Models/Actor.cs
@@ -14,15 +14,26 @@ namespace Auth0.AuthenticationApi.Models;
 public class Actor
 {
     /// <summary>
+    /// Initializes a new <see cref="Actor"/>. Used by the deserializer to populate the
+    /// read-only properties; the type is otherwise only produced by decoding an access token.
+    /// </summary>
+    [JsonConstructor]
+    public Actor(string? subject, Actor? act)
+    {
+        Subject = subject;
+        Act = act;
+    }
+
+    /// <summary>
     /// The <c>sub</c> of this actor. May be <c>null</c> if the decoded <c>act</c> claim did
     /// not contain a <c>sub</c>.
     /// </summary>
     [JsonPropertyName("sub")]
-    public string? Subject { get; set; }
+    public string? Subject { get; }
 
     /// <summary>
     /// The nested prior actor, or <c>null</c> if this is the end of the delegation chain.
     /// </summary>
     [JsonPropertyName("act")]
-    public Actor? Act { get; set; }
+    public Actor? Act { get; }
 }
